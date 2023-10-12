73855e591095f6e92f1a6e4dddd17c644e43f5cf
astymeilista <143146260+astymeilista@users.noreply.github.com>
2023-10-12 12:14:04 +0700

    DAFTAR HADIR
    Buatlah program untuk absensi sederhana menggunakan metode Switch case, dengan ketentuan user bisa menginputkan nama siswa. Jika nama siswa yang inputkan bernilai benar maka berikan feedback siswa dinyatakan hadir jika sebaliknya maka berikan feedback siswa dinyatakan bolos.

---
+++ loop.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DasarPemrogramanJava;
+
+/**
+ *
+ * @author LAB1
+ */
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        String nama;
+        
+        System.out.println("panggil nama siswa :" );
+        nama = scanner.next();
+        switch (nama) {
+            case "asty":
+                 System.out.println("asty hadir");
+                 break;
+            default:
+                 System.out.println("tidak hadir");
+       }
+    }
+}
