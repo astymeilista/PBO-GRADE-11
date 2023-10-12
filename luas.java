242b2f3d60e7f1cad281b1de2004eecbdd9ebd7c
astymeilista <143146260+astymeilista@users.noreply.github.com>
2023-10-12 12:46:25 +0700

    Tugas Praktek 2_HItung Luas dan Keliling Lingkaran
    Buatlah program menghitung luas dan keliling lingkaran, untuk value dari jari - jari atau diameter silakan ditentukan sendiri. Dibebaskan mau pakai nilai phi 22/7 atau  3,14

---
+++ luas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DasarPemrogramanJava;
+
+/**
+ *
+ * @author LAB1
+ */
+public class luas {
+    public static void main(String[] args) {
+      double phi = 22/7;
+         int r = 40;
+                 
+                 
+         double l;
+         l = phi * r * r;
+         System.out.print("hasil luasA lingkaran = " + l); 
+    }
+}
